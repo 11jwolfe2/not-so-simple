644619551e6f7f279c4c83c61bb62a145983e345
jackwolfeiii <jwolfe723@gmail.com>
2025-02-06 17:01:14 -0500

    update footer and packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import { Inter } from "next/font/google"
 import type React from "react"
 import Navigation from "@/components/Navigation"
+import Footer from "@/components/Footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Navigation />
         {children}
       </body>
+      <Footer />
     </html>
   )
 }
